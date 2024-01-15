8979df573f30a77bd875cf356ac640c2a96d1fbb
ASUS <ASUS@LAPTOP-O8PUBDR5>
2024-01-15 20:07:29 +0530

    add files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import './app.css'
+import Link from 'next/link'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function KryptoBirdMarketplace({Component, pageProps}) {
+  return (
+    <div>
+      <nav className='border-b p-6' style={{backgroundColor:'red'}}>
+        <p className='text-4x1 font-bold text-white'>NFT Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+        <Link href='/'className='mr-4'>Main Marketplace</Link>
+            
+           
+          <Link href='/mint-item'
+             className='mr-6'>
+              Mint Tokens
+            
+          </Link>
+          <Link href='/my-nfts'
+             className='mr-6'>
+              My NFts
+            
+          </Link>
+          <Link href='/account-dashboard'
+             className='mr-6'>
+              Account Dashboard
+            
+          </Link>
+          </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+export default KryptoBirdMarketplace 
